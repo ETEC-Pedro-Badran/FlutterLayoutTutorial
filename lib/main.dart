318dd5a8cdfaa30515dcd1d56d2a14fe6cc769fd
EVALDO FERNANDES RÉU JÚNIOR <EVALDO@DESKTOP-8UMJN9H>
2020-10-13 20:27:31 -0300

    Tutorial Flutter Layout Linha de Títulos

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,60 @@ void main() {
   runApp(HelloWorld());
 }
 
+
+
+
+
 class HelloWorld extends StatelessWidget {
+  
+  Widget titleSection = Container(
+    
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+        children:[
+          Expanded(
+            
+            child: Column(
+              
+              crossAxisAlignment: CrossAxisAlignment.start,
+               children:[
+                 Container(
+                   padding: EdgeInsets.only(bottom:8.0),
+                   child: Text("Oeschinen Lake Campground",style:TextStyle(fontWeight:FontWeight.w900)),
+                          
+                 ),
+                 Text("Kandersteg, Switzerland",
+                     style: TextStyle(color: Colors.grey[500])),
+               ],
+            )
+          ),
+          
+          Icon(Icons.star,
+               color: Colors.red,
+              ),
+          
+          Text("41"),
+          
+        ]
+    ),
+    
+  );
+  
+  
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello World",
-      home: Scaffold(),
+      title: "Demontação de Layout",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+      
+        appBar: AppBar(
+          title: Text("Demontação de Layout"),
+        ),
+        body: Column(children:[
+          titleSection,
+        ] ),
+        
+      ),
     );
   }
 }
